05a1612a3e031982b608a801ecf44442bb7e6f88
waleed naseem <waleed.naseem1@gmail.com>
2020-09-09 06:25:28 +0500

    .

---
+++ website/src/Component/Header.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import { Navbar , Nav, FormControl, NavDropdown} from 'react-bootstrap'
+
+export default class Header extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <Navbar bg="dark" variant="dark" >
+                        <Navbar.Brand href="#home">Logo</Navbar.Brand>
+                    <Nav className="col-sm-4">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#features">Features</Nav.Link>
+                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    </Nav>
+                    <form className="col-sm-6">
+                        <input type="text" placeholder="Search" className="mr-auto" />
+                        <button className="btn-primary">Search</button>
+                    </form>
+                    <Nav >
+                        <Nav.Link href="#">cart</Nav.Link>
+                    </Nav>
+                    <NavDropdown title="Accounts" href="#">
+                        <NavDropdown.Item>first</NavDropdown.Item>
+                        <NavDropdown.Item>second</NavDropdown.Item>
+                        <NavDropdown.Item>third</NavDropdown.Item>
+                        <NavDropdown.Item>forth</NavDropdown.Item>
+                    </NavDropdown>
+                </Navbar>
+            </React.Fragment>
+        )
+    }
+}
---
+++ website/src/Website.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Header from './Component/Header'
+
+export default class Website extends Component {
+  render() {
+    return (
+     <React.Fragment>
+       <Header />
+     </React.Fragment>
+    )
+  }
+}
