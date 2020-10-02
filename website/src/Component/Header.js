956027b621ebd2ed360fba25232e44e82400d9b1
waleed naseem <waleed.naseem1@gmail.com>
2020-10-02 12:35:49 +0500

    .

--- website/src/Component/Header.js
+++ website/src/Component/Header.js
@@ -1,34 +1,47 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import "./header.css"
-import SearchIcon from "@material-ui/icons/Search"
+// import { Button } from '@material-ui/core'
+import { Search } from '@material-ui/icons'
+
+
 export default class Header extends Component {
     
     render() {
         return (
-            <div>
-               <nav style={{backgroundColor : "white"}}>
-                    <Link path="#">
-                    <img style={header} src="images/logo.jpg" alt="logo" />
-                    </Link>
-                    
-                    <input style={input_field}  type="text" placeholder="search" />
-                    <SearchIcon style={button} />
-               </nav>
+            <div style={headers}>
+                <Link path="/">
+                <img style={Logo} src="images/logo.jpg" alt="logo" />
+                </Link>
+
+                <div style={header__search}>
+                <input style={header__searchInput} type="text" placeholder="search" />
+                <Search style={header_searchIcon} />
+               </div>
+
             </div>
         )
     }    
     
 }
-const header ={
-   height : '100px',
-   padding: '5px 0px ',
+const Logo ={
+   height : '60px',
+
 }
-const input_field={
-    padding : '2px',
-    border : '10px 0px'
+const header__searchInput={
+    height : '12px',
+    padding: '10px',
+    border:'none',
+    width:'100%',
 }
-const button = {
+const header_searchIcon = {
     backgroundColor: 'brown',
-    padding: '20px',
+    padding: '1px',
+}
+const headers={
+    backgroundColor: 'black',
+    alignItems: 'center',
+}
+const header__search={
+    display : 'flex',
 }
