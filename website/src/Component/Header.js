ac76c4edb6973b85bd650249bd7a1cbd9ec66973
waleed naseem <waleed.naseem1@gmail.com>
2020-10-08 07:56:22 +0500

    .

--- website/src/Component/Header.js
+++ website/src/Component/Header.js
@@ -6,17 +6,45 @@ import { Search } from '@material-ui/icons'
 
 export default class Header extends Component {
     
-    render() {
+    render() { 
         return (
             <div style={headers}>
                 <Link path="/">
-                <img style={Logo} src="images/logo.jpg" alt="logo" />
+                {/* <img style={Logo} src="images/logo.jpg" alt="logo" /> */}
+                <img style={Logo} src="http://pngimg.com/uploads/amazon/amazon_PNG25.png" alt="logo" />
                 </Link>
 
                 <div style={header__search}>
                 <input style={header__searchInput} type="text" placeholder="search" />
                 <Search style={header_searchIcon} />
                </div>
+               <div style={NavHeader}>
+                   <Link to="/login" style={HeaderLink}>
+                    <div style={HeaderOptions}>
+                    <span style={headoption1}>hello waleed</span>
+                    <span style={headOption2}>Sign in</span>
+                    </div>
+                   </Link>
+                   <Link to="/" style={HeaderLink}>
+                    <div style={HeaderOptions}>
+                    <span style={headoption1}>return</span>
+                    <span style={headOption2}>& order</span>
+                    </div>
+                   </Link>
+                   <Link to="/" style={HeaderLink}>
+                    <div style={HeaderOptions}>
+                    <span style={headoption1}>your</span>
+                    <span style={headOption2}>prime</span>
+                    </div>
+                   </Link>
+                   <Link to="/" style={HeaderLink}>
+                    <div style={HeaderOptions}>
+                    <span style={headoption1}>your</span>
+                    <span style={headOption2}>prime</span>
+                    </div>
+                   </Link>
+               </div>
+               
 
             </div>
         )
@@ -24,24 +52,47 @@ export default class Header extends Component {
     
 }
 const Logo ={
-   height : '50px',
-   
+   height : '40px',
+   marginLeft: '05px', 
+   marginRight: '18px', 
+//    marginTop: '20px', 
 }
 const headers={
     backgroundColor: 'black',
     alignItems: 'center',
+    display:'flex'
 }
 const header__search={
-           display : 'inline-block', 
-           padding: '5px'
+           display : 'flex',
+           flex: '1',
 }
 const header__searchInput={
-    height : '12px',
+    height : '30px',
     padding: '10px',
     border:'none',
     width:'100%',
 }
 const header_searchIcon = {
     backgroundColor: 'brown',
-    // padding: '1px',
+    padding: '4px',
+    height: '30px',
+    display:'flex'
+}
+const NavHeader ={
+    display : 'flex',
+    justifyContent : 'space-evenly'
+}
+const HeaderLink={
+    color : 'white',
+    textDecoration: 'none'
+}
+const HeaderOptions={
+    display :'flex',
+    flexDirection: 'column'
+}
+const headoption1={
+
+}
+const headOption2={
+
 }
