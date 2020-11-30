4e4a2c3abb4831ed0977a48f260057d9840b0ac1
waleed naseem <waleed.naseem1@gmail.com>
2020-11-30 17:07:42 +0500

    .

--- materialui/src/Forms.js
+++ materialui/src/Forms.js
@@ -4,8 +4,8 @@ function Forms(props) {
     return (
         <div>
             <form>
-                <input type="text"  value={props.FirstName} onChange={e => props.setName({FirstName: e.target.value})} placeholder="First Name"/>
-                <input type="text"  value={props.LastName} onChange={e => props.setName({LastName: e.target.value})} placeholder="Last Name"/>
+                <input type="text"  value={props.FirstName} onChange={e => props.setName({LastName: props.LastName,FirstName: e.target.value})} placeholder="First Name"/>
+                <input type="text"  value={props.LastName} onChange={e => props.setName({FirstName: props.FirstName,LastName: e.target.value})} placeholder="Last Name"/>
                 <h3>your full name is {props.FirstName+" "+props.LastName}</h3>
             </form>
         </div>
