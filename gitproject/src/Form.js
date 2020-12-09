d7dffbfd5016419c1e10a36357dec0054370fa52
waleed naseem <waleed.naseem1@gmail.com>
2020-12-09 15:39:44 +0500

    almost completed

--- gitproject/src/Form.js
+++ gitproject/src/Form.js
@@ -2,26 +2,46 @@ import React from 'react'
 
 export default function Form({userInfo,submit,search}) {
     return (
-        <div style={mainDiv}>
-            <div style={formdiv}>
-                <form onSubmit={submit}>
-                    <input type='search' style={inputsearch} placeholder='search here' onChange={search} />
-                    <div style={oneDiv}>
-                        <img src={userInfo.avatar} style={image}/>
-                        <div>
-                            hi
+            <div style={mainDiv}>
+                <div style={div100}>
+                    <div style={div50}>
+                        <div style={formdiv}>
+                            <form onSubmit={submit}>
+                                <input type='search' style={inputsearch} placeholder='search here' onChange={search} />
+                                <div style={oneDiv}>
+                                    <img src={userInfo.avatar} style={image}/>
+                                    <div>
+                                    <h5 style={name}>Name : {userInfo.name}</h5>
+                                    </div>
+                                    <div style={div100}>
+                                    <h4 style={div50}>Followers :{userInfo.followers}</h4> 
+                                        <h4 style={div50}>Following :{userInfo.following}</h4>
+                                    </div>
+                                </div>
+                            </form>
                         </div>
                     </div>
-                </form>
+                    <div style={div50}>
+                        {userInfo.respo}
+                    </div>
+                </div>
             </div>
-        </div>
             
         
     )
 }
+const div100={
+    width: '100%',
+    display:'inline-block'
+}
+const div50={
+    width:'49%',
+    display:'inline-block',
+}
 const name={
     color: 'black',
-    fontSize:'26px'
+    fontSize:'26px',
+    display:'flex'
 }
 const image={
     display:'flex',
@@ -36,9 +56,8 @@ const formdiv={
     alignContent: 'center',
     display:'flex',
     flexDirection: 'column',
-    maxWidth: '50%',
+    maxWidth: '100%',
     margin:'auto',
-    padding: 'auto',
 }
 const inputsearch={
     width:'50%',
@@ -52,7 +71,7 @@ const inputsearch={
     borderLeft:'0px',
 }
 const oneDiv={
-    border:'0px solid gray',
+    border:'1px solid gray',
     width:'50%',
     hieght:'500px',
     margin:'auto',
