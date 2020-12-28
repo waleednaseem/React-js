b5b7fb5095ca9d7af338ccda4a53278a507904e8
waleed naseem <waleed.naseem1@gmail.com>
2020-12-28 19:14:25 +0500

    .

--- materialui/src/Context Api/Stateprovider.js
+++ materialui/src/Context Api/Stateprovider.js
@@ -1,18 +1,3 @@
-import React,{createContext ,useState} from 'react'
-import nanoid from 'nanoid'
+import {createcontext} from 'react'
 
-export const noteContext = createContext([])
-
-export const noteProvider=({children})=>{
-    const [note, setNote]=useState([])
-
-    const AddNote =(text)=>{
-        setNote(...note,{text,id: nanoid()})
-    }
-    const removeNote =(id)=>{
-        setNote(note.filter(note.id != id))
-    }
-    return<noteContext.Provider value={{note,AddNote,removeNote}}>
-        {children}
-    </noteContext.Provider>
-}
+export const StateProvider=createcontext({})
